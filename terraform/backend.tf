2148e43da153edf67d5759c35ffac7bb26797dc3
roberto.cardenas <rcardenas20@gmail.com>
2024-04-05 23:51:34 -0400

    adding backend to project

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,8 @@
-backend "azurerm" {
-    resource_group_name = var.bkstrgrg
-    storage_account_name = var.bkstrg
-    container_name = var.bkcontainer
-    key = var.bkstrgkey
+terraform {
+	backend "azurerm" {
+		resource_group_name = var.bkstrgrg
+		storage_account_name = var.bkstrg
+		container_name = var.bkcontainer
+		key = var.bkstrgkey
+	}
 }
