9327c32926c5607c454308e86c2ea7ee7ab8201c
roberto.cardenas <rcardenas20@gmail.com>
2024-04-07 10:44:02 -0400

    (feat) creating the acr and role assignment

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -30,11 +30,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
-  # sensitive = true
+  sensitive = true
 }
 
 # Need this for a non-prod environment to be able to configure local kubectl
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
-  # sensitive = true
+  sensitive = true
 }
