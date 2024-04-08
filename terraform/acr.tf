404b381c0b92e7aa0e9d06c4eeb8e2803ff257a0
roberto.cardenas <rcardenas20@gmail.com>
2024-04-08 12:55:05 -0400

    avoiding this issue - need to use command line

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -16,13 +16,13 @@ resource "azurerm_container_registry" "acr" {
   sku                 = local.acr.sku
 }
 
-resource "azurerm_role_assignment" "role_aks_acr" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-  depends_on = [ 
-    azurerm_kubernetes_cluster.aks, 
-    azurerm_container_registry.acr 
-  ]
-}
+# resource "azurerm_role_assignment" "role_aks_acr" {
+#   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+#   role_definition_name             = "AcrPull"
+#   scope                            = azurerm_container_registry.acr.id
+#   skip_service_principal_aad_check = true
+#   depends_on = [ 
+#     azurerm_kubernetes_cluster.aks, 
+#     azurerm_container_registry.acr 
+#   ]
+# }
